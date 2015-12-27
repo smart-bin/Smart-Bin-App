c6e2e8009d8d52c98c997f9eef2553709a24694a
Ian Wensink <ian@burst-digital.com>
2015-12-27 13:20:31 +0100

    Print bin types from database + save bin functionality

--- smart-bin/www/js/add-bin.js
+++ smart-bin/www/js/add-bin.js
@@ -1,168 +1,52 @@
 function initAddBin() {
     $(".mdl-layout__content").on("scroll", checkFixedHeader);
     $(".back-button").on("click", back);
-    initSnackbar(showSnackbar);
-    $(".save-bin").on("click", function () {
-        saveSnackbar({
-            message: "Bin saved"
-        });
-    });
-    printBinTypes([
-        {
-            name: "Restafval",
-            key: 0
-        },
-        {
-            name: "Plastic",
-            key: 1
-        },
-        {
-            name: "Glas",
-            key: 2
-        },
-        {
-            name: "Organisch",
-            key: 3
-        },
-        {
-            name: "Tin",
-            key: 4
-        },
-        {
-            name: "Papier",
-            key: 5
-        },
-        {
-            name: "Chemisch",
-            key: 6
-        }
-    ]);
-}
-
-function processBinAPI(bin) {
-    printBinInfo(bin);
-}
-
-function printBinInfo(bin) {
-    $("#bin-header h1").text(bin.Name);
-    var batteryImage = "";
-    var batteryStatus = "";
-    var batteryStatusColor = "rgb(0, 0, 0)";
-    if (bin.BatteryLevel <= 10) {
-        batteryImage = "5";
-        batteryStatus = "Critical";
-        batteryStatusColor = "rgb(255, 0, 0)";
-    } else if (bin.BatteryLevel > 10 && bin.BatteryLevel <= 25) {
-        batteryImage = "20";
-        batteryStatus = "Low";
-    } else if (bin.BatteryLevel > 25 && bin.BatteryLevel <= 40) {
-        batteryImage = "30";
-        batteryStatus = "Moderate";
-    } else if (bin.BatteryLevel > 40 && bin.BatteryLevel <= 60) {
-        batteryImage = "50";
-        batteryStatus = "Fine";
-    } else if (bin.BatteryLevel > 60 && bin.BatteryLevel <= 85) {
-        batteryImage = "70";
-        batteryStatus = "Good";
-    } else if (bin.BatteryLevel > 85) {
-        batteryImage = "100";
-        batteryStatus = "High";
-    }
-    $("#bin-header").css({
-        "background-image": "url(img/types/glass_360x180.png)",
-        "background-color": "#82ba73"
-    });
-    $("#battery-level").css("background-image", "url(img/battery/battery_square_" + batteryImage + ".png)");
-    $("#battery-level #battery-status").text(batteryStatus + " (" + Math.round(bin.BatteryLevel) + "%)").css("color", batteryStatusColor);
-}
-
-function processBinHistory(history) {
-    var passedMonths = [];
-    var now = new Date();
-    var minUnix = now.setMonth(now.getMonth() - 6) / 1000;
-    $.each(history, function () {
-        if (this.UnixTimestamp > minUnix) {
-            passedMonths.push(this);
-        }
-    })
-    processGraph(passedMonths);
-    processStatistics(history);
-}
-
-function processGraph(history) {
-    var graph = {};
-    var dataByMonth = {};
-    graph.labels = [];
-    graph.data = [];
-    $.each(history, function () {
-        var timestamp = new Date(this.UnixTimestamp * 1000);
-        var month = timestamp.getMonth() + 1;
-        var nameMonth = getNameMonth(month);
-        if (!dataByMonth.hasOwnProperty(month)) {
-            dataByMonth[month] = {
-                name: nameMonth,
-                month: month,
-                weight: 0
-            };
-            graph.labels.push(month);
-        }
-        dataByMonth[month].weight += this.Weight;
-    });
-    graph.datasets = [{
-        label: "Bin graph",
-        fillColor: "rgba(169,170,166,0.5)",
-        strokeColor: "rgba(220,220,220,1)",
-        pointColor: "rgba(220,220,220,1)",
-        pointStrokeColor: "#fff",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(220,220,220,1)"
-    }];
-    $.each(dataByMonth, function () {
-        graph.data.push(this.weight);
-    });
-    graph.datasets[0].data = graph.data;
-    graph.labels = graph.labels.sort(function(a,b){return a - b});
-    for (var i = 0, l = graph.labels.length; i < l; i++) {
-        graph.labels[i] = getNameMonth(graph.labels[i]);
-    }
-    printGraph(graph);
-}
-
-function printGraph(graph) {
-    var graphEl = $("#graph canvas")[0].getContext("2d");
-    var myLineChart = new Chart(graphEl).Line(graph);
-}
-
-function processStatistics(history) {
-    var totalWeight = 0;
-    var firstTimestamp = new Date().getTime();
-    $.each(history, function () {
-        var timestamp = new Date(this.UnixTimestamp * 1000);
-        if (timestamp < firstTimestamp) firstTimestamp = timestamp;
-        totalWeight += this.Weight;
-    });
-    var averageWeight = totalWeight / ((new Date().getTime() - firstTimestamp) / 2635200000);
-    $("#total-weight").text("Totaal: " + totalWeight + " kg");
-    $("#average-weight").text("Gemiddeld: " + Math.round(averageWeight) + " kg/m");
+    $("#select-picture-click").on("click", initSelectPicture);
+    $(".save-bin").on("click", saveBin);
+    API.getBinTypes(printBinTypes);
 }
 
 function printBinTypes(bins) {
     var bintypes = "";
     var first = true;
-    $.each(bins, function () {
-        bintypes += "<label class=\"mdl-radio mdl-js-radio mdl-js-ripple-effect form-item\" for=\"option-" + this.key + "\">" +
-                        "<input type=\"radio\" id=\"option-" + this.key + "\" class=\"mdl-radio__button\" name=\"bintype\" value=\"" + this.key + "\"" + (first?" checked":"") + ">" +
-                        "<span class=\"mdl-radio__label\">" + this.name + "</span>" +
+    $.each(bins, function (k, v) {
+        bintypes += "<label class=\"mdl-radio mdl-js-radio mdl-js-ripple-effect form-item\" for=\"option-" + k + "\">" +
+                        "<input type=\"radio\" id=\"option-" + k + "\" class=\"mdl-radio__button\" name=\"bintype\" value=\"" + k + "\"" + (first?" checked=\"checked\"":"") + ">" +
+                        "<span class=\"mdl-radio__label\">" + v + "</span>" +
                     "</label>";
         if (first) first = false;
     });
     $(".main-form").append(bintypes);
+    componentHandler.upgradeAllRegistered();
 }
 
+function saveBin() {
+    var name = $("#name").val();
+    var type = $("input[name=bintype]:checked").val();
+    if (name != "" && typeof name !== typeof undefined && type != "" && typeof type !== typeof undefined) {
+        API.registerNewBin(getUserId(), name, type, function (bin) {
+            createBin(bin, name, type);
+        });
+    }
+}
 
+function createBin(bin, name, type) {
+    console.log(bin);
+    console.log(name + " " + type);
+    binSuccess(bin.BinId);
+}
 
+function binSuccess(binId) {
+    saveSnackbar({
+        message: "<i class=\"material-icons mdl-color-text--green\">check_circle</i><span>Bin saved</span>"
+    });
+    //location.href = "bin.html?id=" + binId;
+}
 
+function initSelectPicture() {
+    console.log("pick photo");
 
+}
 
 
 
