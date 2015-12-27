42d14f21f4f9a8922803437b76c2ed6a3c7e9cd8
Ian Wensink <ian@burst-digital.com>
2015-12-27 23:34:38 +0100

    Show per bin this month's weight on bins.html

--- smart-bin/www/js/bins.js
+++ smart-bin/www/js/bins.js
@@ -3,23 +3,44 @@ function initBins() {
 }
 
 function processBins(output) {
-    console.log(output.Bins);
     $.each(output.Bins, function () {
         var card = {
             id: this.BinId,
             time: "Nu net",
-            title: this.Name,
+            title: truncateText(this.Name, 25),
             subtitle1: "<i class=\"material-icons\">battery_full</i><span>" + this.BatteryLevel + "% batterij</span>",
-            subtitle2: "<i class=\"material-icons\">assessment</i><span>" + "" + " kg deze maand</span>",
+            subtitle2: "<i class=\"material-icons\">assessment</i><span>" + getTotalWeightThisMonth(this.BinId) + " kg deze maand</span>",
             button1Link: "bin.html?id=" + this.BinId,
-            cardClasses: "bin-type-card bin-type--" + convertBinTypeToClass(this.Type.TypeId),
+            cardClasses: "two-supporting-texts bin-type-card bin-type--" + convertBinTypeToClass(this.Type.TypeId),
             type: "bin",
             button2: false
         };
-
-        console.log(card);
         $("#bins").append(formatCard(card));
     });
+}
+
+function getTotalWeightThisMonth(binId) {
+    var weight = 0;
+    $.ajax({
+        dataType: "JSON",
+        method:"GET",
+        async: false,
+        url: API.apiBaseUrl + "history.php?id=" + binId + "&from=0&to=0",
+        success: function(history)
+        {
+            var totalWeight = 0;
+            var thisMonth = new Date();
+            thisMonth = new Date(thisMonth.getTime() - (thisMonth.getDate() * 86400000) - (thisMonth.getHours() * 3600000)).getTime();
+            console.log(thisMonth);
+            $.each(history, function () {
+                var timestamp = new Date(this.UnixTimestamp * 1000).getTime();
+                console.log(this.BinID + " " + this.Date + " kg: " + this.Weight);
+                if (timestamp > thisMonth) totalWeight += this.Weight;
+            });
+            weight = totalWeight;
+        }
+    });
+    return weight;
 }
 
 
