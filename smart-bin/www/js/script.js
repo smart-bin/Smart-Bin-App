b3aee60c14db72bd47dcc85691a75bb0579c8474
Ian Wensink <ian@burst-digital.com>
2015-12-27 20:59:42 +0100

    Created auto reload function for development

--- smart-bin/www/js/script.js
+++ smart-bin/www/js/script.js
@@ -5,6 +5,7 @@ function initApp() {
     initSnackbar(showSnackbar);
     API.language = "nl";
     API.getUser(getUserId(), "info", saveUser);
+    autoreload();
 }
 
 function saveUser(output) {
@@ -202,6 +203,41 @@ function convertBinTypeToClass(type) {
     }
 }
 
+
+function autoreload() {
+
+        var url = 'http://' + document.location.host + '/__api__/autoreload';
+
+        function postStatus() {
+            var xhr = new XMLHttpRequest();
+            xhr.open('post', url, true);
+            xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+            xhr.onreadystatechange = function() {
+                if (this.readyState === 4 && /^[2]/.test(this.status)) {
+                }
+            }
+            xhr.send();
+        }
+
+        function checkForReload() {
+            var xhr = new XMLHttpRequest();
+            xhr.open('get', url, true);
+            xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+            xhr.onreadystatechange = function() {
+                if (this.readyState === 4 && /^[2]/.test(this.status)) {
+                    var response = JSON.parse(this.responseText);
+                    if (response.content.outdated) {
+                        postStatus();
+                        window.location.reload();
+                    }
+                }
+            }
+            xhr.send();
+        }
+
+        setInterval(checkForReload, 100);
+
+}
 
 
 
