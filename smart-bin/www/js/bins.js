6f662eba7ba4144107bdf7fd697322459177d1e9
Ian Wensink <ian@burst-digital.com>
2015-12-28 15:10:09 +0100

    DONE: Make totalweight feature on bins.html async

--- smart-bin/www/js/bins.js
+++ smart-bin/www/js/bins.js
@@ -3,47 +3,43 @@ function initBins() {
 }
 
 function processBins(output) {
+    var cards = "";
+    $("#loader.spinner").hide();
     $.each(output.Bins, function () {
         var card = {
             id: this.BinId,
             time: "Nu net",
             title: truncateText(this.Name, 25),
             subtitle1: "<i class=\"material-icons\">battery_full</i><span>" + this.BatteryLevel + "% batterij</span>",
-            subtitle2: "<i class=\"material-icons\">assessment</i><span>" + getTotalWeightThisMonth(this.BinId) + " kg deze maand</span>",
+            subtitle2: "<i class=\"material-icons\">assessment</i><span><span class=\"weight-this-month\">?</span> kg deze maand</span>",
             button1Link: "bin.html?id=" + this.BinId,
             cardClasses: "two-supporting-texts bin-type-card bin-type--" + convertBinTypeToClass(this.Type.TypeId),
             type: "bin",
             button2: false
         };
-        $("#bins").append(formatCard(card));
+        cards += formatCard(card);
+    });
+    $("#bins").append(cards);
+    $.each($(".card-bin"), function () {
+        var binId = $(this).attr("id");
+        binId = binId.substr(5, binId.length);
+        API.getEntireHistory(binId, processTotalWeightThisMonth);
     });
 }
 
-function getTotalWeightThisMonth(binId) {
-    var weight = 0;
-    $.ajax({
-        dataType: "JSON",
-        method:"GET",
-        async: false,
-        url: API.apiBaseUrl + "history.php?id=" + binId + "&from=0&to=0",
-        success: function(history)
-        {
-            var totalWeight = 0;
-            var thisMonth = new Date();
-            thisMonth = new Date(thisMonth.getTime() - (thisMonth.getDate() * 86400000) - (thisMonth.getHours() * 3600000)).getTime();
-            console.log(thisMonth);
-            $.each(history, function () {
-                var timestamp = new Date(this.UnixTimestamp * 1000).getTime();
-                console.log(this.BinID + " " + this.Date + " kg: " + this.Weight);
-                if (timestamp > thisMonth) totalWeight += this.Weight;
-            });
-            weight = totalWeight;
-        }
+function processTotalWeightThisMonth(history) {
+    var totalWeight = 0;
+    var thisMonth = new Date();
+    thisMonth.setDate(1);
+    thisMonth.setHours(0);
+    thisMonth.setMinutes(0);
+    thisMonth.setSeconds(0);
+    thisMonth = thisMonth.getTime();
+    $.each(history.History, function () {
+        var timestamp = new Date(this.UnixTimestamp * 1000).getTime();
+        if (timestamp > thisMonth) totalWeight += this.Weight;
     });
-    return weight;
+    console.log(totalWeight);
+    $("#card-" + history.BinId + " .weight-this-month").text(totalWeight);
 }
-
-
-
-
 
